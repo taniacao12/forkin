e724ed23b8b2ac9f55f7213e56fba893ab1d144e
Tania Cao <tcaosu@stuy.edu>
2018-11-19 00:28:35 -0500

    add files

--- main.c
+++ main.c
@@ -3,17 +3,27 @@
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
 
 int main(){
   printf("START FORKING!\n");
+  pid_t childA, childB;
   srand(time(NULL));
   printf("parent pid: %d\n", getppid());
-  fork();
-  while (getppid() != -1) {
-    printf("parent pid: %d\n", getppid());
-    printf("child pid: %d\n", getpid());
-    sleep(rand() % 15 + 5);
+  childA = fork();
+  if (childA)
+    childB = fork();
+  if (childA == 0 || childB == 0) {
+    printf("child pid : %d\n", getpid());
+    int num = sleep(rand() % 15 + 5);
+    printf("Child %d slept for %d seconds", getpid(), num);
   }
+
+  int status, child;
+  child = wait(&status);
+  printf("parent %d knows child %d slept for %d seconds\n", getpid(), child, WEXITSTATUS(status));
   
   return 0;
 }
